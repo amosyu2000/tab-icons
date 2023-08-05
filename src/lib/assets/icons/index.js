3437144b670107d2a6f00cfbbabbf0129ecb31f5
Amos Yu <yua25@mcmaster.ca>
2023-08-05 00:26:52 -0400

    dynamically import icons

--- src/lib/assets/icons/index.js
+++
@@ -1,7 +0,0 @@
-import folder from './Folder.ico'
-import svelte from './Svelte.ico'
-
-export default {
-	folder : { path: folder, name: "Folder" },
-	svelte : { path: svelte, name: "Svelte" },
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script>
 	import { page } from '$app/stores'
-	import icons from '$lib/assets/icons'
 
-	$: hash = $page.url.hash.slice(1)
+	// import all icons from assets
+	const icons = {}
+	const iconModules = import.meta.glob('$lib/assets/icons/*.ico', { eager: true })
+	for (const path in iconModules) {
+		const name = path.substring(0, path.lastIndexOf(".")).split('/').pop()
+		const id = name.toLowerCase().split(" ").join("-")
+		icons[id] = {path, name}
+	}
+
+	$: hash = decodeURIComponent($page.url.hash.slice(1))
 
 	$: tabIcon = icons[hash] || {}
 </script>
